6e9ce47db13673d6a42f54aa91ecbddfb5fbb3c5
Martirio Artificial <otrogatodel71@gmail.com>
2025-07-07 12:29:59 +0000

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="paquete",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="martirioartificial",                         # Tu nombre
+    author_email="otrogatodel71@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/martirioartificial/paquete",     # URL del proyecto
+)
